65aa2190cefe98c06ba90f6826a89075a05814d5
Henhen <imammuldani@gmail.com>
2024-06-12 19:11:49 +0700

    add data plants

---
+++ src/data/plants.ts
@@ -0,0 +1,12 @@
+export const dataPlants = [
+  { id: 1, name: "Tomat" },
+  { id: 2, name: "Cabai Merah" },
+  { id: 3, name: "Terong" },
+  { id: 4, name: "Jahe" },
+  { id: 5, name: "Serai" },
+  { id: 6, name: "Jambu" },
+  { id: 7, name: "Pakcoy" },
+  { id: 8, name: "Kangkung" },
+  { id: 9, name: "Cabai Rawit" },
+  { id: 10, name: "Pandan" },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+import { dataPlants } from "./data/plants";
 
-export default app
+app.get("/", (c) => {
+  return c.json({ message: "This is EzFarm API" });
+});
+
+app.get("/plants", (c) => {
+  return c.json(dataPlants);
+});
+
+app.get("/plants/:id", (c) => {
+  const id = c.req.param("id");
+
+  if (!id) {
+    return c.json({ message: "Invalid ID" });
+  }
+
+  const plant = dataPlants.find((plant) => plant.id === parseInt(id));
+
+  if (!plant) {
+    return c.json({ message: "Plant not found" });
+  }
+
+  return c.json(plant);
+});
+
+export default app;
