095172e7405521e3023256fa806b9e0576ee5ce7
Henhen <imammuldani@gmail.com>
2024-06-19 10:21:21 +0700

    add create, delete, put

--- src/data/plants.ts
+++ src/data/plants.ts
@@ -1,12 +1,102 @@
-export const dataPlants = [
-  { id: 1, name: "Tomat" },
-  { id: 2, name: "Cabai Merah" },
-  { id: 3, name: "Terong" },
-  { id: 4, name: "Jahe" },
-  { id: 5, name: "Serai" },
-  { id: 6, name: "Jambu" },
-  { id: 7, name: "Pakcoy" },
-  { id: 8, name: "Kangkung" },
-  { id: 9, name: "Cabai Rawit" },
-  { id: 10, name: "Pandan" },
+export type Plant = {
+  id: number;
+  name: string;
+  scientificName: string;
+  type: "Vegetable" | "Herb" | "Fruit";
+  sunlight: string;
+  watering: string;
+  harvestTime: string;
+};
+
+export const dataPlants: Plant[] = [
+  {
+    id: 1,
+    name: "Tomato",
+    scientificName: "Solanum lycopersicum",
+    type: "Vegetable",
+    sunlight: "Full sun",
+    watering: "Regular",
+    harvestTime: "60-80 days",
+  },
+  {
+    id: 2,
+    name: "Red Chili",
+    scientificName: "Capsicum annuum",
+    type: "Vegetable",
+    sunlight: "Full sun",
+    watering: "Moderate",
+    harvestTime: "70-90 days",
+  },
+  {
+    id: 3,
+    name: "Eggplant",
+    scientificName: "Solanum melongena",
+    type: "Vegetable",
+    sunlight: "Full sun",
+    watering: "Moderate",
+    harvestTime: "100-150 days",
+  },
+  {
+    id: 4,
+    name: "Ginger",
+    scientificName: "Zingiber officinale",
+    type: "Herb",
+    sunlight: "Partial shade",
+    watering: "Regular",
+    harvestTime: "8-10 months",
+  },
+  {
+    id: 5,
+    name: "Lemongrass",
+    scientificName: "Cymbopogon citratus",
+    type: "Herb",
+    sunlight: "Full sun",
+    watering: "Moderate",
+    harvestTime: "75-100 days",
+  },
+  {
+    id: 6,
+    name: "Guava",
+    scientificName: "Psidium guajava",
+    type: "Fruit",
+    sunlight: "Full sun",
+    watering: "Moderate",
+    harvestTime: "2-4 years",
+  },
+  {
+    id: 7,
+    name: "Bok Choy",
+    scientificName: "Brassica rapa subsp. chinensis",
+    type: "Vegetable",
+    sunlight: "Partial shade",
+    watering: "Regular",
+    harvestTime: "30-45 days",
+  },
+  {
+    id: 8,
+    name: "Water Spinach",
+    scientificName: "Ipomoea aquatica",
+    type: "Vegetable",
+    sunlight: "Full sun to partial shade",
+    watering: "Regular",
+    harvestTime: "30-60 days",
+  },
+  {
+    id: 9,
+    name: "Bird's Eye Chili",
+    scientificName: "Capsicum frutescens",
+    type: "Vegetable",
+    sunlight: "Full sun",
+    watering: "Moderate",
+    harvestTime: "90-120 days",
+  },
+  {
+    id: 10,
+    name: "Pandan",
+    scientificName: "Pandanus amaryllifolius",
+    type: "Herb",
+    sunlight: "Partial shade",
+    watering: "Regular",
+    harvestTime: "6-12 months",
+  },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { Hono } from "hono";
+import { type Plant, dataPlants } from "./data/plants";
+
+let plants = dataPlants;
 
 const app = new Hono();
 
-import { dataPlants } from "./data/plants";
-
 app.get("/", (c) => {
-  return c.json({ message: "This is EzFarm API" });
+  return c.json({
+    message: "This is EzFarm API",
+    plants: "/plants",
+  });
 });
 
 app.get("/plants", (c) => {
-  return c.json(dataPlants);
+  if (plants.length === 0) {
+    return c.json({ message: "No plants found" });
+  }
+
+  return c.json(plants);
 });
 
 app.get("/plants/:id", (c) => {
@@ -19,13 +27,99 @@ app.get("/plants/:id", (c) => {
     return c.json({ message: "Invalid ID" });
   }
 
-  const plant = dataPlants.find((plant) => plant.id === parseInt(id));
+  const plant = plants.find((plant) => plant.id === parseInt(id));
 
   if (!plant) {
     return c.json({ message: "Plant not found" });
   }
 
   return c.json(plant);
+});
+
+app.delete("/plants", (c) => {
+  plants = [];
+  return c.json({ message: "All plants are deleted", plants });
+});
+
+app.post("/plants", async (c) => {
+  const body = await c.req.json();
+
+  if (!body) {
+    return c.json({ message: "Invalid body" });
+  }
+
+  const newPlant: Plant = {
+    id: plants.length + 1,
+    name: body.name,
+    scientificName: body.scientificName,
+    type: body.type,
+    sunlight: body.sunlight,
+    watering: body.watering,
+    harvestTime: body.harvestTime,
+  };
+
+  console.log(newPlant);
+
+  plants = [...plants, newPlant];
+
+  return c.json({ message: "Plant added", plant: newPlant });
+});
+
+app.delete("/plants/:id", (c) => {
+  const id = c.req.param("id");
+
+  if (!id) {
+    return c.json({ message: "Invalid ID" });
+  }
+
+  const plant = plants.find((plant) => plant.id === parseInt(id));
+
+  if (!plant) {
+    return c.json({ message: "Plant not found" });
+  }
+
+  plants = plants.filter((plant) => plant.id !== parseInt(id));
+
+  return c.json({ message: `Plant with ${id} has been deleted` });
+});
+
+app.put("/plants/:id", async (c) => {
+  const id = c.req.param("id");
+
+  if (!id) {
+    return c.json({ message: "Invalid ID" });
+  }
+
+  const plant = plants.find((plant) => plant.id === parseInt(id));
+
+  if (!plant) {
+    return c.json({ message: "Plant not found" });
+  }
+
+  const body = await c.req.json();
+  const newPlant: Plant = {
+    id: plant.id,
+    name: body.name,
+    scientificName: body.scientificName,
+    type: body.type,
+    sunlight: body.sunlight,
+    watering: body.watering,
+    harvestTime: body.harvestTime,
+  };
+
+  const updatedPlant = plants.map((plant) => {
+    if (plant.id === parseInt(id)) {
+      return newPlant;
+    }
+    return plant;
+  });
+
+  plants = updatedPlant;
+
+  return c.json({
+    message: `Plant with ${id} has been updated`,
+    plant: newPlant,
+  });
 });
 
 console.log("Server is running on port 3000");
